5a424703b88ae5a273f11a1a1496b229c07ab11f
Ebtesam Harith <ebtesam@github.com>
2017-12-13 12:09:01 +0300

    comment

--- app/src/main/java/com/example/android/mathquize/MainActivity.java
+++ app/src/main/java/com/example/android/mathquize/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,41 +26,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void CheckBox(View view) {
 
-        boolean checked = ((CheckBox) view).isChecked();
+       
+
+        CheckBox ch1=findViewById(R.id.checkboxQuestion3_1);
+        CheckBox ch2=findViewById(R.id.checkboxQuestion3_2);
+        CheckBox ch3=findViewById(R.id.checkboxQuestion3_3);
 
 
-        switch (view.getId()) {
-            case R.id.checkboxQuestion3_1:
-                if (checked) {
-                    Toast.makeText(this, "Correct!! Good jop", Toast.LENGTH_SHORT).show();
-                    score = score + 1;
-                    break;
-                }
-            case R.id.checkboxQuestion3_2:
-                if (checked) {
-                    Toast.makeText(this, "Correct!! Good jop", Toast.LENGTH_SHORT).show();
-                    score = score + 1;
-                    break;
-                }
-            case R.id.checkboxQuestion4_1:
-                if (checked) {
-                    Toast.makeText(this, "Correct!! Good jop", Toast.LENGTH_SHORT).show();
-                    score = score + 1;
-                    break;
-                }
-            case R.id.checkboxQuestion4_2:
-                if (checked) {
-                    Toast.makeText(this, "Correct!! Good jop", Toast.LENGTH_SHORT).show();
-                    score = score + 1;
-                    break;
-                }
-            default:
-                Toast.makeText(this, "the answer is false.. try again please", Toast.LENGTH_SHORT).show();
+        if((ch3.isChecked()==true && ch2.isChecked()==true)&& (ch1.isChecked()==false))  {
+            Toast.makeText(this, "Correct!! Good jop", Toast.LENGTH_SHORT).show();
+            score=score+ 1;
+
+        }else
+            Toast.makeText(this, " try again please", Toast.LENGTH_SHORT).show();
 
 
         }
 
-    }
+
 
     public void RadioButton(View view) {
         boolean check = ((RadioButton) view).isChecked();
@@ -105,6 +89,24 @@ public class MainActivity extends AppCompatActivity {
         TextView txt = findViewById(R.id.txtresult);
         String result="The result :  " + String.valueOf(score) + " ";
         txt.setText(result);
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        Reset(view);
+
+    }
+    public void Reset(View view){
+        score=0;
+        RadioGroup radio=findViewById(R.id.radioGroup1);
+        RadioGroup radio2=findViewById(R.id.radioGroup2);
+        radio.clearCheck();
+        radio2.clearCheck();
+        EditText editText =  findViewById(R.id.editTextQuestion5);
+        editText.setText(null);
+        CheckBox ch1=findViewById(R.id.checkboxQuestion3_1);
+        CheckBox ch2=findViewById(R.id.checkboxQuestion3_2);
+        CheckBox ch3=findViewById(R.id.checkboxQuestion3_3);
+        ch1.setChecked(false);
+        ch2.setChecked(false);
+        ch3.setChecked(false);
 
     }
 }
